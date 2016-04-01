ae98fb688762d1b8b81f9ac42bfe261eae7c45f5
Sindormir.net <sindormir@sindormir.net>
2016-04-01 13:14:44 +0200

    Cambios menores

--- tema5/tema5.ino
+++ tema5/tema5.ino
@@ -8,9 +8,9 @@ void setup() {
 
 void loop() {
 
-  while(digitalRead(PULSADOR) == LOW);
+  while(digitalRead(PULSADOR) == LOW) { }
 
-  while(1) {
+  while(1 == 1) {
     digitalWrite(LED, HIGH);
     delay(100);
     digitalWrite(LED, LOW);
