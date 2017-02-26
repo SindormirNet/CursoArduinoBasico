233261adbfd6a5f98a87042ec086ea4a8646ce08
Jorge Gómez <syvic@sindormir.net>
2017-02-26 16:58:25 +0100

    Mejora de legilibilidad del código

--- tema9/tema9.ino
+++ tema9/tema9.ino
@@ -1,19 +1,19 @@
 void setup(){
   Serial.begin(9600); //Indicamos la velocidad del puerto serie 
-  pinMode(9, OUTPUT); //Establecemos el pin 9 como salida
+  pinMode(6, OUTPUT); //Establecemos el pin 9 como salida
 
   Serial.print("Introduce números del 0 al 9: ");
 }
 
 void loop() {
-  byte dato;
-  byte brillo;
+  char dato;
+  int brillo;
 
   if (Serial.available() > 0) { //¿Ha enviado algún dato el PC?
     dato = Serial.read();  //Guardamos el dato enviado en la variable dato        
-    brillo = map(dato,0x30,0x39,0,255); //Escalamos el valor
-    analogWrite(9, brillo);  //Modificamos la intensidad del LED 
+    brillo = map(dato,'0','9',0,255); //Escalamos el valor
+    analogWrite(6, brillo);  //Modificamos la intensidad del LED 
     Serial.print("Cambiado brillo a ");  //Escribimos de vuelta al PC
-    Serial.println(dato-0x30);
+    Serial.println(dato);
   }
 }
