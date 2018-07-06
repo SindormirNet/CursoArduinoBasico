9d85bd8d1a6ad825dcef4c478ee045f5011ba5b9
Jorge Gómez <syvic@sindormir.net>
2018-07-06 11:26:30 +0200

    Update tema9.ino

--- tema9/tema9.ino
+++ tema9/tema9.ino
@@ -1,6 +1,8 @@
+#define LED A5
+
 void setup(){
   Serial.begin(9600); //Indicamos la velocidad del puerto serie 
-  pinMode(6, OUTPUT); //Establecemos el pin 9 como salida
+  pinMode(LED, OUTPUT); //Establecemos el pin 9 como salida
 
   Serial.print("Introduce números del 0 al 9: ");
 }
@@ -12,7 +14,7 @@ void loop() {
   if (Serial.available() > 0) { //¿Ha enviado algún dato el PC?
     dato = Serial.read();  //Guardamos el dato enviado en la variable dato        
     brillo = map(dato,'0','9',0,255); //Escalamos el valor
-    analogWrite(6, brillo);  //Modificamos la intensidad del LED 
+    analogWrite(LED, brillo);  //Modificamos la intensidad del LED 
     Serial.print("Cambiado brillo a ");  //Escribimos de vuelta al PC
     Serial.println(dato);
   }
